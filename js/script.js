a8d1347f7619190f6560748809c63a3f5095c38a
Gabianchini <gabibisdx@gmail.com>
2023-11-06 18:06:13 -0300

    Add redirect navbar

--- js/script.js
+++ js/script.js
@@ -117,3 +117,16 @@ setTimeout(function(){
 },250);
 
 });
+
+//Scroll
+// When the user scrolls down 20px from the top of the document, slide down the navbar
+// When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+}
